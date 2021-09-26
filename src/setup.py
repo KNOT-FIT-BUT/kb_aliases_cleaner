f3da216928697f65f4bc6b03bfc8d3b32d7c1777
Daniel Kříž <Daniel.kriz@protonmail.com>
2021-09-26 22:44:09 +0200

    add kb as python module

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='kb',
+    version='0.1',
+    description='Library that contains functions to interact with kb',
+    author='Tomáš Volf, Daniel Kříž',
+    author_email='ivolf@vutbr.cz, xkrizd03@vutbr.cz',
+    packages=['kb'],
+    zip_safe=False
+)
