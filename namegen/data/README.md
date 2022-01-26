0b6cb7a92cd4533dd3ecc9e802fa0251f49273fb
Daniel Kříž <Daniel.kriz@protonmail.com>
2022-01-26 01:15:57 +0100

    add namegen as submodule

--- namegen/data/README.md
+++
@@ -1,2 +0,0 @@
-# Složka data
-Tato složka obsahuje složku s jazyky. Pokud chcete vědět více k samotným jazykům, tak informace najdete v README.md v dané složce.
--- namegen/data/languages/en/eq_gen.py
+++
@@ -1,5 +0,0 @@
-{
-    "P:::M": [{"St.", "Saint"}, {"Sr.", "sr.", "senior"}, {"Jr.", "jr.", "junior"}],
-    "P:::F": [{"St.", "Saint"}, {"Sr.", "sr.", "senior"}, {"Jr.", "jr.", "junior"}],
-    "L": [{"St.", "Saint"}, {"Sr.", "sr.", "senior"}, {"Jr.", "jr.", "junior"}],
-}
